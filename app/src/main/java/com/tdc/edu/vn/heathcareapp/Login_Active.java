f61a3a8d090daa865379484af89da7a5f78645f4
levannghia <levannghia.tdc2018@gmail.com>
2020-11-19 21:28:56 +0700

    Khoi tao dao dien

---
+++ app/src/main/java/com/tdc/edu/vn/heathcareapp/Login_Active.java
@@ -0,0 +1,17 @@
+package com.tdc.edu.vn.heathcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Login_Active extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_login);
+    }
+}
---
+++ app/src/main/java/com/tdc/edu/vn/heathcareapp/Registration_Active.java
@@ -0,0 +1,14 @@
+package com.tdc.edu.vn.heathcareapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration_Active extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_registration);
+    }
+}
