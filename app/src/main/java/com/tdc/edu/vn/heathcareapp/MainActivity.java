f0107d8e7593b7abcec4f487cd3d10ea715b9eb9
macintoshhd <khoinguyenvk9@gmail.com>
2020-11-20 17:10:55 +0700

    Complete Navigation Bar Bottom

--- app/src/main/java/com/tdc/edu/vn/heathcareapp/MainActivity.java
+++ app/src/main/java/com/tdc/edu/vn/heathcareapp/MainActivity.java
@@ -1,14 +1,53 @@
 package com.tdc.edu.vn.heathcareapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setControl();
+        setEvent();
     }
+
+    private void setEvent() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new WorkoutsFragment()).commit();
+    }
+
+    private void setControl() {
+        bottomNavigationView = findViewById(R.id.BottomNavView);
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.NewFeed:
+                    fragment = new NewFeedFragment();
+                    break;
+                case R.id.Nutrition:
+                    fragment = new NutritionFragment();
+                    break;
+                case R.id.Workouts:
+                    fragment = new WorkoutsFragment();
+                    break;
+                case R.id.More:
+                    fragment = new MoreFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
+        }
+    };
 }
