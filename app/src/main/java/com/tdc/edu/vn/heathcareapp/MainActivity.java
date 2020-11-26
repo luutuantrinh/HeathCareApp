cefc5e4800344ec396dff5f8c08b2fd93244ce9b
macintoshhd <khoinguyenvk9@gmail.com>
2020-11-26 07:50:11 +0700

    gg

--- app/src/main/java/com/tdc/edu/vn/heathcareapp/MainActivity.java
+++ app/src/main/java/com/tdc/edu/vn/heathcareapp/MainActivity.java
@@ -1,23 +1,26 @@
 package com.tdc.edu.vn.heathcareapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setControl();
         setEvent();
+
     }
 
     private void setEvent() {
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void setControl() {
         bottomNavigationView = findViewById(R.id.BottomNavView);
+
     }
 
     BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,11 +41,16 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.NewFeed:
                     fragment = new NewFeedFragment();
+                    getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+                    getSupportActionBar().setCustomView(R.layout.toolbar_newfeed);
                     setTitle("New Feed");
                     break;
                 case R.id.Nutrition:
                     fragment = new NutritionFragment();
+                    getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+                    getSupportActionBar().setCustomView(R.layout.toolbar_nutrition);
                     setTitle("Nutrition");
+
                     break;
                 case R.id.Progress:
                     fragment = new ProgressFragment();
